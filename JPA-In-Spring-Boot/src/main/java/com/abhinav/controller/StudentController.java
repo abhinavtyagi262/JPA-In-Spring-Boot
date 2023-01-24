f3df1de0b81ffb312859f68b8d54f16e13daa42c
Abhinav Tyagi <61839001+abhinavtyagi262@users.noreply.github.com>
2023-01-24 12:36:47 +0530

    Update StudentController.java

--- JPA-In-Spring-Boot/src/main/java/com/abhinav/controller/StudentController.java
+++ JPA-In-Spring-Boot/src/main/java/com/abhinav/controller/StudentController.java
@@ -4,6 +4,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,7 @@ import lombok.extern.slf4j.Slf4j;
  *
  */
 @Slf4j
+@CrossOrigin
 @RestController
 public class StudentController {
 	
